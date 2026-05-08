537fd5df0897ff1947fdcf23a428c004462aa1c1
solmi <js5185204@gmail.com>
2026-05-09 08:45:35 +0900

    packs

--- packs.py
+++ packs.py
@@ -5,8 +5,48 @@ PACKS = [
         "filename": "ru-v1.0.0.zip",
         "tag": "v1.0.0",
         "corpus": [
-            {"name": "OpenSubtitles"},
-            {"name": "Wikipedia"}
+            {"Data source": "Leipzig Corpora Collection, University of Leipzig"},
+            {"Corpora used": "rus_wikipedia_2021_300K, rus-ru_web-public_2019_1M"}
+        ]
+    },
+    {
+        "lang": "de",
+        "version": "1.0.0",
+        "filename": "de-v1.0.0.zip",
+        "tag": "v1.0.0",
+        "corpus": [
+            {"Data source": "Leipzig Corpora Collection, University of Leipzig"},
+            {"Corpora used": "deu_wikipedia_2021_300K, deu-de_web_2021_1M"}
+        ]
+    },
+    {
+        "lang": "en",
+        "version": "1.0.0",
+        "filename": "en-v1.0.0.zip",
+        "tag": "v1.0.0",
+        "corpus": [
+            {"Data source": "Leipzig Corpora Collection, University of Leipzig"},
+            {"Corpora used": "eng-simple_wikipedia_2021_300K, eng-uk_web-public_2018_1M"}
+        ]
+    },
+    {
+        "lang": "ko",
+        "version": "1.0.0",
+        "filename": "ko-v1.0.0.zip",
+        "tag": "v1.0.0",
+        "corpus": [
+            {"Data source": "Leipzig Corpora Collection, University of Leipzig"},
+            {"Corpora used": "kor_wikipedia_2021_300K, kor-kr_web_2020_1M"}
+        ]
+    },
+    {
+        "lang": "sr",
+        "version": "1.0.0",
+        "filename": "sr-v1.0.0.zip",
+        "tag": "v1.0.0",
+        "corpus": [
+            {"Data source": "Leipzig Corpora Collection, University of Leipzig"},
+            {"Corpora used": " srp_wikipedia_2021_300K, srp-rs_web_2016_1M"}
         ]
     },
 ]
